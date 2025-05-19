b04b98b8eb465276e7b9ccc066c2d8417da4f907
FullStackDj <full.stackdj@gmail.com>
2025-05-19 21:03:10 +0300

    Implemented unit tests for basic shopping cart logic

---
+++ SportsStore.Tests/CartPageTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using SportsStore.Models;
+using SportsStore.Pages;
+
+namespace SportsStore.Tests;
+
+public class CartPageTests
+{
+    [Fact]
+    public void CanLoadCart()
+    {
+        Product product1 = new Product { ProductID = 1, Name = "Product1" };
+        Product product2 = new Product { ProductID = 2, Name = "Product2" };
+        Mock<IStoreRepository> mockRepo = new();
+        mockRepo.Setup(m => m.Products).Returns((new Product[]
+        {
+            product1, product2
+        }).AsQueryable<Product>());
+
+        Cart testCart = new();
+        testCart.AddItem(product1, 2);
+        testCart.AddItem(product2, 1);
+
+        Mock<ISession> mockSession = new();
+        byte[] data = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(testCart));
+        mockSession.Setup(c => c.TryGetValue(It.IsAny<string>(), out data));
+        Mock<HttpContext> mockContext = new();
+        mockContext.SetupGet(c => c.Session).Returns(mockSession.Object);
+
+        CartModel cartModel = new(mockRepo.Object)
+        {
+            PageContext = new(new ActionContext
+            {
+                HttpContext = mockContext.Object,
+                RouteData = new RouteData(),
+                ActionDescriptor = new PageActionDescriptor()
+            })
+        };
+        cartModel.OnGet("myUrl");
+
+        Assert.Equal(2, cartModel.Cart?.Lines.Count());
+        Assert.Equal("myUrl", cartModel.ReturnUrl);
+    }
+
+    [Fact]
+    public void CanUpdateCart()
+    {
+        Mock<IStoreRepository> mockRepo = new();
+        mockRepo.Setup(m => m.Products).Returns((new Product[]
+        {
+            new Product { ProductID = 1, Name = "Product1" }
+        }).AsQueryable<Product>());
+        Cart? testCart = new();
+        Mock<ISession> mockSession = new();
+        mockSession.Setup(s => s.Set(It.IsAny<string>(), It.IsAny<byte[]>()))
+            .Callback<string, byte[]>((key, val) =>
+            {
+                testCart = JsonSerializer.Deserialize<Cart>(Encoding.UTF8.GetString(val));
+            });
+        Mock<HttpContext> mockContext = new();
+        mockContext.SetupGet(c => c.Session).Returns(mockSession.Object);
+
+        CartModel cartModel = new(mockRepo.Object)
+        {
+            PageContext = new PageContext(new ActionContext
+            {
+                HttpContext = mockContext.Object,
+                RouteData = new RouteData(),
+                ActionDescriptor = new PageActionDescriptor()
+            })
+        };
+        cartModel.OnPost(1, "myUrl");
+
+        Assert.Single(testCart.Lines);
+        Assert.Equal("Product1", testCart.Lines.First().Product.Name);
+        Assert.Equal(1, testCart.Lines.First().Quantity);
+    }
+}
---
+++ SportsStore.Tests/CartTests.cs
@@ -0,0 +1,94 @@
+﻿using SportsStore.Models;
+
+namespace SportsStore.Tests;
+
+public class CartTests
+{
+    [Fact]
+    public void CanAddNewLines()
+    {
+        Product product1 = new Product { ProductID = 1, Name = "Product1" };
+        Product product2 = new Product { ProductID = 2, Name = "Product2" };
+
+        Cart target = new();
+
+        target.AddItem(product1, 1);
+        target.AddItem(product2, 1);
+        CartLine[] results = target.Lines.ToArray();
+
+        Assert.Equal(2, results.Length);
+        Assert.Equal(product1, results[0].Product);
+        Assert.Equal(product2, results[1].Product);
+    }
+
+    [Fact]
+    public void CanAddQuantityForExistingLines()
+    {
+        Product product1 = new Product { ProductID = 1, Name = "Product1" };
+        Product product2 = new Product { ProductID = 2, Name = "Product2" };
+
+        Cart target = new();
+
+        target.AddItem(product1, 1);
+        target.AddItem(product2, 1);
+        target.AddItem(product1, 10);
+        CartLine[] results = (target.Lines ?? new())
+            .OrderBy(c => c.Product.ProductID).ToArray();
+
+        Assert.Equal(2, results.Length);
+        Assert.Equal(11, results[0].Quantity);
+        Assert.Equal(1, results[1].Quantity);
+    }
+
+    [Fact]
+    public void CanRemoveLine()
+    {
+        Product product1 = new Product { ProductID = 1, Name = "Product1" };
+        Product product2 = new Product { ProductID = 2, Name = "Product2" };
+        Product product3 = new Product { ProductID = 3, Name = "Product3" };
+
+        Cart target = new();
+
+        target.AddItem(product1, 1);
+        target.AddItem(product2, 3);
+        target.AddItem(product3, 5);
+        target.AddItem(product2, 1);
+
+        target.RemoveLine(product2);
+
+        Assert.Empty(target.Lines.Where(c => c.Product == product2));
+        Assert.Equal(2, target.Lines.Count());
+    }
+
+    [Fact]
+    public void CalculateCartTotal()
+    {
+        Product product1 = new Product { ProductID = 1, Name = "Product1", Price = 80M };
+        Product product2 = new Product { ProductID = 2, Name = "Product2", Price = 30M };
+
+        Cart target = new();
+
+        target.AddItem(product1, 1);
+        target.AddItem(product2, 1);
+        target.AddItem(product1, 3);
+        decimal result = target.ComputeTotalValue();
+
+        Assert.Equal(110M, result);
+    }
+
+    [Fact]
+    public void CanClearContents()
+    {
+        Product product1 = new Product { ProductID = 1, Name = "Product1", Price = 120M };
+        Product product2 = new Product { ProductID = 2, Name = "Product2", Price = 60M };
+
+        Cart target = new();
+
+        target.AddItem(product1, 1);
+        target.AddItem(product2, 1);
+
+        target.Clear();
+
+        Assert.Empty(target.Lines);
+    }
+}
