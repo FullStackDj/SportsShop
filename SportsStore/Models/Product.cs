197b8ce3c97f450a69101d0cd44b300e53b8fdc1
FullStackDj <full.stackdj@gmail.com>
2025-05-25 15:23:18 +0300

    Added list, validation and data handling in admin panel

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,15 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace SportsStore.Models;
 
 public class Product
 {
     public long? ProductID { get; set; }
+    
+    [Required(ErrorMessage = "product name")]
     public string Name { get; set; } = String.Empty;
+    
+    [Required(ErrorMessage = "description")]
     public string Description { get; set; } = String.Empty;
 
+    [Required]
+    [Range(0.01, double.MaxValue, ErrorMessage = "positive price")]
     [Column(TypeName = "decimal(8, 2)")] 
     public decimal Price { get; set; }
 
+    [Required(ErrorMessage = "category")]
     public string Category { get; set; } = String.Empty;
 }
