ba451552b4f44550755a69a302b61b7a6ebc5cd9
FullStackDj <full.stackdj@gmail.com>
2025-05-27 12:56:08 +0300

    Added admin user seeding and authorized display page

---
+++ SportsStore/Models/IdentitySeedData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models;
+
+public static class IdentitySeedData
+{
+    private const string adminUser = "Admin";
+    private const string adminPassword = "pa$$word";
+
+    public static async void EnsurePopulated(IApplicationBuilder app)
+    {
+        AppIdentityDbContext context = app.ApplicationServices
+            .CreateScope().ServiceProvider
+            .GetRequiredService<AppIdentityDbContext>();
+        if (context.Database.GetPendingMigrations().Any())
+        {
+            context.Database.Migrate();
+        }
+
+        UserManager<IdentityUser> userManager = app.ApplicationServices
+            .CreateScope().ServiceProvider
+            .GetRequiredService<UserManager<IdentityUser>>();
+
+        IdentityUser user = await userManager.FindByNameAsync(adminUser);
+        if (user == null)
+        {
+            user = new IdentityUser("Admin");
+            user.Email = "admin@admin.com";
+            user.PhoneNumber = "123-456";
+            await userManager.CreateAsync(user, adminPassword);
+        }
+    }
+}
